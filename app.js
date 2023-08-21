203fa97645eea65bc4390766a45e790e0ee3f802
branden1155 <bpmurphy@student.fullsail.edu>
2023-08-20 21:22:35 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+require("dotenv").config("./env")
+
+const http = require("http");
+
+//------ Variables Being used for Submission ------
+
+//This is the shared Secret using .env...
+const hostname = process.env.HOST_NAME;
+const port = process.env.PORT_NUMBER;
+
+//shared Secret example...
+const secret = process.env.SECRET_KEY;
+
+const fs = require("fs")
+let directory_name= "./"
+let filenames = fs.readdirSync(directory_name)
+
+
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+
+    f = ""
+    filenames.forEach((file) =>{
+        f = f+file+" \n";
+    })
+
+    res.end(f)
+
+});
+
+console.log(secret);
+
+server.listen(port, hostname, ()=>{})
